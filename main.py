2b3d8b838e64d876cf8a9880940aeeaeb137c580
Adairvlz <vcda03@gmail.com>
2025-03-03 13:18:03 -0600

    optimizacion del main

--- main.py
+++ main.py
@@ -3,21 +3,17 @@ import procesos  # Código del Estudiante 1
 import cpu       # Código del Estudiante 2
 import analisis  # Código del Estudiante 3
 
-# Configuración inicial
-RANDOM_SEED = 42
-MEMORY_CAPACITY = 100  # Puede cambiarse en las pruebas
-INTERVALO_LLEGADA = 10  # Se cambiará en diferentes escenarios
-TOTAL_PROCESOS = 25  # Se probará con 25, 50, 100, 150, 200 procesos
-RANDOM_SEED = 42
-CPU_VELOCIDAD = 3  # Instrucciones por unidad de tiempo (puede ser 3 o 6)
+# Pedir parámetros al usuario
+MEMORY_CAPACITY = int(input("Ingrese la capacidad de RAM: "))
+INTERVALO_LLEGADA = int(input("Ingrese el intervalo de llegada de los procesos: "))
+TOTAL_PROCESOS = int(input("Ingrese la cantidad total de procesos: "))
+CPU_VELOCIDAD = int(input("Ingrese la velocidad del CPU (instrucciones por unidad de tiempo): "))
 
-#Iniciar el entorno
+# Iniciar el entorno
 env = simpy.Environment()
 ram = simpy.Container(env, init=MEMORY_CAPACITY, capacity=MEMORY_CAPACITY)
 cola_procesos = []  # Cola compartida con cpu.py
 cPu = simpy.Resource(env, capacity=1)  # Un solo CPU, se puede modificar
-
-
 
 # Iniciar simulación con procesos generados
 env.process(procesos.iniciar_simulacion(env, ram, TOTAL_PROCESOS, INTERVALO_LLEGADA, cola_procesos))
@@ -37,7 +33,3 @@ for proceso in cola_procesos:
 env.run()
 
 
-
-
-# Llamar al análisis de datos y generar gráficas
-#analisis.generar_graficas()
